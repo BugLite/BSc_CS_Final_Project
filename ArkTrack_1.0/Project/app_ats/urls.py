9a17f1983140e38256ba4aa17930f0906e8606b8
Hyder Ali <ali121onine@gmail.com>
2024-08-05 14:19:02 +0500

    Basic UI setup

--- ArkTrack_1.0/Project/app_ats/urls.py
+++ ArkTrack_1.0/Project/app_ats/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"), 
+    path('', views.home, name="home"), 
     path('webcam/', views.display_webcam, name='display_webcam'),
     path('webcam_feed/', views.webcam_feed, name='webcam_feed'),
     path('motion_detector/', views.display_motion_detector, name='display_motion_detector'),
--- ArkTrack_1.0/Project/app_ats/views.py
+++ ArkTrack_1.0/Project/app_ats/views.py
@@ -4,19 +4,21 @@ from .detector_main import gen_frames
 from .detector_sub import generate_motion_frames
 
 # Create your views here.
-
 def home(request):
-    return HttpResponse("Hello Unaiza!")
+    return render(request, 'home.html')
 
+# Requried: It's responsible for generating and sending the actual video content to the client. 
+# Without this function, there would be no mechanism to capture and stream the webcam feed to the browser.
 def webcam_feed(request):
     return StreamingHttpResponse(gen_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
 
+def motion_detector_view(request):
+    return StreamingHttpResponse(generate_motion_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+# Required: It provides a user interface (UI) for viewing the stream. This function essentially serves the web page that the user interacts with. 
+# Without this function, users wouldn't have a webpage to visit where they could see the motion detection stream
 def display_webcam(request):
     return render(request, 'webcam.html')
-
-def motion_detector_view(request):
-    # Directly call the generator function for StreamingHttpResponse
-    return StreamingHttpResponse(generate_motion_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 def display_motion_detector(request):
     return render(request, 'motion_detector.html')
