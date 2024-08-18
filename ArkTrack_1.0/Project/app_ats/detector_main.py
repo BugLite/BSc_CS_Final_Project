baf1806170ec5b0ad7f0843cb2c02490da5caece
Hyder Ali <ali121onine@gmail.com>
2024-08-18 14:48:42 +0500

    soc - record videos commit

--- ArkTrack_1.0/Project/app_ats/detector_main.py
+++ ArkTrack_1.0/Project/app_ats/detector_main.py
@@ -8,6 +8,8 @@ from django.utils.timezone import now
 from app_ats.models import RecordedVideo
 from django.conf import settings
 
+from app_ats.motion_recorder import record_videos
+
 from app_ats.alerts import send_alert_mail
 
 
@@ -110,23 +112,9 @@ def gen_frames():
             if time.time() - recording_start_time >= max_recording_duration:
                 recording = False
                 print(f"Recording stopped: {video_file_name}")
-                # Saving video using ffmpeg
-                out = ffmpeg.input('pipe:', framerate=20, format='rawvideo', pix_fmt='bgr24', s=f'{frame_1.shape[1]}x{frame_1.shape[0]}')
-                out = ffmpeg.output(out, video_file_name, pix_fmt='yuv420p')
-                out = ffmpeg.overwrite_output(out)
-                out.run(input=np.concatenate(frames_list, axis=0).tobytes())
-                frames_list = []
 
-                # Save video metadata to the database
-                captured_video = RecordedVideo.objects.create(
-                    title=os.path.basename(video_file_name),
-                    # file path is relative to 'MEDIA_ROOT' in Database
-                    file_path=os.path.relpath(video_file_name, start=settings.MEDIA_ROOT),
-                    duration=timedelta(seconds=max_recording_duration),
-                    recorded_timestamp=now()
-                )
-                print(f"Video saved and metadata stored: {video_file_name}")
-
+                captured_video = record_videos(frames_list, recording_count, frame_1.shape, max_recording_duration)
+                recording_count += 1
                 send_alert_mail(captured_video)
 
         # Encode frame as JPEG
---
+++ ArkTrack_1.0/Project/app_ats/motion_recorder.py
@@ -0,0 +1,31 @@
+import os
+import ffmpeg
+import numpy as np
+from datetime import timedelta
+from django.utils.timezone import now
+from django.conf import settings
+from app_ats.models import RecordedVideo
+
+def record_videos(frames_list, video_id, frame_shape, max_duration):
+    records_dir = os.path.join(settings.MEDIA_ROOT, 'recordings')
+    os.makedirs(records_dir, exist_ok=True)
+
+    video_title = os.path.join(records_dir, f'recording_id({video_id}).mp4')
+
+    # Saving video using ffmpeg
+    out = ffmpeg.input('pipe:', framerate=20, format='rawvideo', pix_fmt='bgr24', s=f'{frame_shape[1]}x{frame_shape[0]}')
+    out = ffmpeg.output(out, video_title, pix_fmt='yuv420p')
+    out = ffmpeg.overwrite_output(out)
+    out.run(input=np.concatenate(frames_list, axis=0).tobytes())
+    
+    # Save video metadata to the database
+    captured_video = RecordedVideo.objects.create(
+        title=os.path.basename(video_title),
+        # file path is relative to 'MEDIA_ROOT' in Database
+        file_path=os.path.relpath(video_title, start=settings.MEDIA_ROOT),
+        duration=timedelta(seconds=max_duration),
+        recorded_timestamp=now()
+    )
+    print(f"Video saved and metadata stored: {video_title}")
+
+    return captured_video
