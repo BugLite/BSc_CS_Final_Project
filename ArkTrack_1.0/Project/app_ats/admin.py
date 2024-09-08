67fc888a99106afabcb2bf9a8b14c4b9ac5b83b9
Hyder Ali <ali121onine@gmail.com>
2024-09-08 15:55:32 +0500

    admin commit

--- ArkTrack_1.0/Project/app_ats/admin.py
+++ ArkTrack_1.0/Project/app_ats/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import RecordedVideo
 
-# Register your models here.
+class ArkTrackAdmin(admin.ModelAdmin):
+    """
+    A customised admin interface for management of the ArkTrack System Recordings.
+    """
+    list_display = ('title', 'file_path', 'duration', 'recorded_timestamp')
+    search_fields = ('title', 'file_path')
+    ordering = ('-recorded_timestamp',)
+
+# registering my RecordedVideo model with the admin site
+admin.site.register(RecordedVideo, ArkTrackAdmin)
