8b291f27d91ad296b4f54309f1ef8a4a7558ea5f
Hyder Ali <ali121onine@gmail.com>
2024-09-09 11:11:11 +0500

    final cleanup commit

--- ArkTrack_1.0/Project/app_ats/static/js/main.js
+++ ArkTrack_1.0/Project/app_ats/static/js/main.js
@@ -1,4 +1,4 @@
-// Bulma Script for Navbar Burger
+// bulma navbar burger
 document.addEventListener('DOMContentLoaded', () => {
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
@@ -14,26 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// javascript time
+// displaying Pakistan Standard Time
 function updateTime() {
     const now = new Date();
     const day = now.getDate().toString().padStart(2, '0');
-    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
     const year = now.getFullYear();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    
-    // Format date and time
     const dateString = `${day}-${month}-${year}`;
     const timeString = `| PKT ${hours}:${minutes}:${seconds}`;
     
-    // Combine date and time
     document.getElementById('current-time').textContent = `${dateString} ${timeString}`;
 }
-
-// Update time every second
 setInterval(updateTime, 1000);
-
-// Initialize time on page load
 document.addEventListener('DOMContentLoaded', updateTime);
